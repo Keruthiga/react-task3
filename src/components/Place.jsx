0ba3af7fab610e790ac0cd23994d27f02187c43e
Keruthiga <krithy08.me@.com>
2023-08-24 23:14:09 +0530

    changes

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -9,7 +9,7 @@ const Place = () => {
         {   
             heading:"01.Chennai",
             imgSrc: require('../Home/Chennai.jpeg'),
-            url:"01.Chennai",
+            url:"chennai",
             title: '"The Detroit Of India"',
             description1:"Formerly known as Madras, Chennai is the capital city of the state of Tamil Nadu, in the southern part of India. Located on the Coromandel coast of Bay of Bengal, Chennai is as dynamic as it is immersed in tradition. This 'capital of the south', is one among the four metropolitan siblings of India, having a rich cultural history which it perfectly balances with its metropolis lifestyle",
             description2:"Amid its chaos of traffic and sweltering humid climate, Chennai is worth visiting for its temples steeped in south-Indian culture, British-era museums and monuments, culinary delights and Marina Beach (Second largest urban beach in the world). Chennai's skyline is famous for its towering skyscrapers, but the heart of Chennai has an old-world charm to it that refuses to be overshadowed."   ,
@@ -40,6 +40,7 @@ const Place = () => {
         {
             heading:`02.Rameshwaram`,
             imgSrc: require('../Home/Rameshwaran.jpeg'),
+            url:"rameshwaram",
             title: '"The Bridge on the Indian Ocean"',
             description1:`Rameshwaram is located on a beautiful island in the South Indian state of Tamil Nadu. It is separated by a small Pamban channel from Sri Lanka. According to Hindu mythology, this is the place where Lord Rama created a bridge across the sea to Sri Lanka.`,
             description2:`Renowned for its magnificent prakaras with massive sculptured pillars on either side, The Ramanathaswamy Temple houses the longest corridor in the world. Agniteertham is famous for its sacred waters and Pilgrims perform poojas in honour of their ancestors at this seashore. The five-faced Hanuman Temple holds the floating stone which was used to build the bridge between India and Sri Lanka. Rameshwaram has the first sea bridge connecting the town of Mandapam with Pamban Island, and Rameswaram.`,
@@ -70,6 +71,7 @@ const Place = () => {
         {
             heading:`03.Kodaikanal`,
             imgSrc: require('../Home/Kodaikanal.jpeg'),
+            url:"kodaikanal",
             title: '"The Princess of Hill Stations"',
             description1:`Kodaikanal Tourism Located in the state of Tamil Nadu, Kodaikanal is one of the most famous honeymoon destinations in India. A Lakeside resort town of Tamil Nadu, Kodaikanal has a beautiful climate, mist-covered manicured cliffs and waterfall that come together to create the ideal setting for a perfect getaway. Kodaikanal means 'the gift of the forests'.`,
             description2:`Nestled amidst the rolling slopes of the Palani Hills, Kodaikanal stands at an altitude of 7200 feet above sea level, and once you visit this hill station, you will find that every bit of what you have imagined it to be is real. Kodaikanal is a place you can go to take a break from the rigours of daily city life, and this hill station lets you sit back and connect with nature as you head out on biking or trekking trails or take a stroll through the vast forests surrounding the town.`,
@@ -100,6 +102,7 @@ const Place = () => {
         {
             heading:`04.Ooty`,
             imgSrc: require('../Home/Ooty.jpeg'),
+            url:'ooty',
             title: '"Queen of the Nilgiris"',
             description1:`Nestled amidst Nilgiri hills, Ooty, also known as Udagamandalam, is a hill station in Tamil Nadu which serves as a top-rated tourist destination. Once regarded as the summer headquarters of the East India Company, the Queen of the hills is a picturesque getaway.`,
             description2:`The Nilgiri mountain railway is the steepest track in all of Asia. Remember the hit song 'Chaiyya Chaiyya' where Shahrukh Khan and Malaika Arora matching steps on top of a train? Remember the breathtaking locales as the train chugged its way across lush greenery? Yes, that was the Nilgiri Mountain Railways, and the Nilgiri Mountains all along Dotted with tea gardens, serene waterfalls, winding country lanes, and charming colonial architecture, Ooty is the perfect respite everyone.`,
@@ -130,6 +133,7 @@ const Place = () => {
         {
             heading:`05.Kanyakumari`,
             imgSrc: require('../Home/Kanyakumari.jpeg'),
+            url:"kanyakumari",
             title: `"Cape Comorin or The Lands End"`
             ,description1:`Kanyakumari Tourism Bordered by the three seas - Arabian, Indian and the Bay of Bengal, Kanyakumari is the southernmost tip of the Indian Peninsula. A small coastal town in the state of Tamil Nadu, Kanyakumari was earlier known as Cape Comorin.`,
             description2:`Kanyakumari offers the marvellous confluence of the Bay of Bengal, the Arabian Sea and the Indian ocean meeting together at a point. But, this is not a miracle, the miracle lies in the beauty that the water of three seas does not mix, you can clearly distinguish between the turquoise blue, deep blue and sea green waters of the three seas. To enjoy the best view of sunset and sunrise, you can visit the Triveni Sangam point and the famous View tower. The town has a mountainous terrain with elevated patches of hills, coconut trees and lined with paddy fields.`,
@@ -160,6 +164,7 @@ const Place = () => {
         {
             heading:`06.Kumbakanom`,
             imgSrc: require('../Home/Kumbakonam.jpeg'),
+            url:'kumbakonam',
             title: '"The Cambridge of India"',
             description1:`Kumbakonam Tourism Sandwiched between two great rivers of southern India, Cauvery and Arsala, Kumbakonam is a gorgeous temple town in the heart of the Thanjavur district of Tamil Nadu. The town is a place for the lovers of history and those seeking to understand India's cultural roots and Hinduism.`,
             description2:`The town is also known for its grand festival called Mahamaham festival which is celebrated every twelve years at the Mahamaham Tank. The town is one of the oldest in Indian history, and known for its famed temples, Chola heritage and also for its marvelous educational institutes. The town is one of the oldest in Indian history, and known for its famed temples, Chola heritage and also for its marvelous educational institutes.`,
@@ -190,6 +195,7 @@ const Place = () => {
         {
             heading:`07.Madurai`,
             imgSrc: require('../Home/Madurai.jpeg'),
+            url:"madurai",
             title: '"The Lotus City"',
             description1:`Madurai Tourism Madurai, the cultural capital of Tamil Nadu, is one of the oldest continuously inhabited cities of India. Ruled by Pandya kings for the longest time in its history, it is called as the 'Lotus City' as it was planned and built in the shape of a lotus. Madurai is known for Meenakshi Amman Temple, dedicated to the goddess Meenakshi with a sanctum for her consort, Sundareshwarar.`,
             description2:`There are many other ancient temples in Madurai, including Thiruparankundram. It is one of the important old temples dedicated to Lord Muruga(Karthikeya) and is located on a hillock approximately 8 km from the city. Having trade ties with ancient Rome, the place holds a great cultural heritage. With bustling bazaars and fantastic street food, Madurai has heritage walks conducted throughout the day.`,
@@ -220,6 +226,7 @@ const Place = () => {
         {
             heading:"08.Yercaud",
             imgSrc: require('../Home/Yarcaud.jpeg'),
+            url:'yercaud',
             title: '"The Land of Seven Forests."',
             description1:`Yercaud Tourism Nestled in the Shivaroy Hills of Tamil Nadu, Yercaud is a hill station in the Eastern Ghats steeped in abundant greenery. Commonly called 'Ooty of the Poor', this region has a history dating back to the time of the British. Located at an altitude of 4970 feet, Yercaud is known for its vast expanses of coffee plantations and great weather.`,
             description2:`Yercaud lake is the main point of attraction of the region.One of the many highlights is the summer festival that takes place in May. It is dedicated to Lord Servarayan, the supreme god of the ranges and gives visitors a glimpse into the rich heritage of this region.`,
@@ -250,6 +257,7 @@ const Place = () => {
         {
             heading:'09.Theni',
             imgSrc: require('../Home/Theni.jpeg'),
+            url:'theni',
             title: '"A little hamlet in Tamil Nadu"',
             description1:`Theni Tourism Dotted by luscious patches of greenery and beautiful waterfalls, Theni is a little hamlet on the Western Ghats. The topography of Theni mainly consists of hills and ranges. It has plenty of rivers and dams and is an abode to 27 forests hence filled with unparalleled greenery.`,
             description2:`There temples like Kamatchi Amman Temple, Vellappar Temple, and Balasubramanya Temple which are brimming with devotees from all around the country throughout the year. The vibrant local markets of Theni are shoppers' paradise. The local handloom products and agricultural products are the best buys at these markets. The aroma filled tea estates in Theni are a paradise. The Kolukkamalai Tea estate is often deemed to be the world's highest organic tea estate. The Suruli Falls and the Kumbakarai Falls are the crown jewels of Theni.`,
@@ -280,6 +288,7 @@ const Place = () => {
         {
             heading:'10.Hogenakkal',
             imgSrc: require('../Home/Hogenakkal.jpeg'),
+            url:"hogenakkal",
             title: `"The one that will take your breath away"`,
             description1:"Hogenakkal Tourism Hogenakkal is a waterfall located in Dharmapuri district of Tamil Nadu where the Kaveri river splits into multiple streams of waterfalls. Located at a distance of 180 km from Bangalore, Hogenakkal has water throughout the year. The carbonite rocks, the coracle (basket boat) rides, freshwater fish, oil massages by locals make it a perfect one day trip or a weekend getaway from Bangalore.",
             description2:`Sometimes referred to as the "Niagara Falls of India," it is also known for the medicinal baths. Also known at Marikottayam, Hoge actually means smoke and Kal means rock. Recently, the place has been found to be littered with plastic bags and garbage and the fish market outside the waterfall might stink. Weekends can be crowded. Keep all these points in mind before visiting.`,
